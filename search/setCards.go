bc46baa293e394e3e415d2328ce60b5985a0451c
dingsky <dingchun_sky@163.com>
2020-08-18 23:27:25 +0800

    fix

--- search/setCards.go
+++ search/setCards.go
@@ -6,22 +6,27 @@ var array, book [10]int64
 var n int64
 
 func setCards(step int64) {
-	if step == n +1 {
+	if step == n  {
 		for _, val := range array {
 			fmt.Printf("%d", val)
 		}
+		fmt.Printf("\n")
+		return
 	}
 
 	var i int64
-	for i=0; i<=n; i++{
-		array[step] = i
-		book[i] = 1
-		setCards(step + 1)
-		book[i] = 0
+	for i=0; i<n; i++{
+		if book[i] == 0 {
+			array[step] = i+1
+			book[i] = 1
+			setCards(step + 1)
+			book[i] = 0
+		}
+
 	}
 }
 
 func main() {
-	n = 10
-	setCards(1)
+	n = 3
+	setCards(0)
 }
