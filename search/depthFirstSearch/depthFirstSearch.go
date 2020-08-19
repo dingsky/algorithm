14b3c51c0eaaa4d6187bc8a29334a1358b7af11a
dingsky <dingchun_sky@163.com>
2020-08-19 23:23:27 +0800

    fix

--- search/depthFirstSearch/depthFirstSearch.go
+++ search/depthFirstSearch/depthFirstSearch.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // map 是一张地图, 如果一个元素为0表示可以通过, 为1表示是障碍物, 为2表示已走过, 9表示目的地
 // 起始位置都为[0,0]
 // 需要返回坐标[0,0]到目的地的最短路径
@@ -15,7 +17,9 @@ var walkStep = [4][2]int64{
 
 
 func GetShortestPath(mapArray [5][5]int64) {
-	walk(&mapArray, 0, 0, 0)
+	var min int64
+	walk(&mapArray, 0, 0, 0, &min)
+	fmt.Printf("%d", min)
 }
 
 
@@ -26,14 +30,21 @@ func walk(mapArray *[5][5]int64, x,y, steps int64, min *int64) {
 	for i:=0; i<4; i++ {
 		x += walkStep[i][0]
 		y += walkStep[i][1]
-		if mapArray[x,y] == 9 {
-
+		if x < 0 || x >= 5 || y < 0 || y >=5 || mapArray[x][y] == 1 || mapArray[x][y] == 2 {continue}
+		if mapArray[x][y] == 9 {
+			if *min == 0 || steps < *min {
+				*min = steps
+			}
+		} else {
+			mapArray[x][y] = 2
+			walk(mapArray, x, y, steps+1, min)
+			mapArray[x][y] = 0
 		}
 	}
 }
 
 func main () {
-	mapArray := [][]int64{
+	mapArray := [5][5]int64{
 		{0,1,0,0,0},
 		{0,0,0,1,0},
 		{0,0,1,9,0},
