24ebbf28f2d765d1b5303bf16dda5e08a5ad7f42
dingsky <dingchun_sky@163.com>
2020-08-19 22:17:04 +0800

    fix

--- search/setCards.go
+++ search/setCards.go
@@ -2,31 +2,43 @@ package main
 
 import "fmt"
 
-var array, book [10]int64
-var n int64
+// 有count个数字, 放进对应的盒子里, 有多少种放法
 
-func setCards(step int64) {
-	if step == n  {
-		for _, val := range array {
-			fmt.Printf("%d", val)
-		}
-		fmt.Printf("\n")
+func SetCards(count int64)[][]int64 {
+
+	result:= make([][]int64, 0)
+	array := make([]int64, count+1)
+	book := make([]int64, count+1)
+	setCards(array, book, &result, 1, count)
+//	fmt.Println(result)
+	return result
+}
+
+
+func setCards(array []int64, book []int64, result *[][]int64,step, count int64) {
+	if step == count + 1  {
+		destArray := make([]int64, len(array)-1)
+		copy(destArray, array[1:])
+		*result = append(*result, destArray)
 		return
 	}
 
 	var i int64
-	for i=0; i<n; i++{
+	for i=1; i<count+1; i++{
 		if book[i] == 0 {
-			array[step] = i+1
+			array[step] = i
 			book[i] = 1
-			setCards(step + 1)
+			setCards(array, book, result, step + 1, count)
 			book[i] = 0
 		}
 
 	}
 }
 
+
 func main() {
-	n = 3
-	setCards(0)
+	result := SetCards(10)
+	for _, val := range result {
+		fmt.Println(val)
+	}
 }
