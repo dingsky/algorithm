817868a7df0e885ec3c155d2a93351c41057d8e4
dingchun <dingchun_sky@163.com>
2020-12-07 19:28:29 +0800

    fix

--- javaSrc/src/sort/InsertSort.java
+++ javaSrc/src/sort/InsertSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.Random;
+
 // 功能: 插入排序
 // 基本思想: 将新的数据插入已有序的序列, 类似抓扑克牌.
 // 时间复杂度: O(N^2)
@@ -7,8 +9,14 @@ package sort;
 public class InsertSort {
     public static void sort(int[] array) {
         int key, j;
+
+        // 遍历没有插入的无序剩余数据
         for(int i=1; i < array.length; i++) {
+
+            // 保存需要插入的数据
             key = array[i];
+
+            // 从有序数据最后一个开始, 如果该数据比插入的数据大, 则往后挪一个, 直到找到一个比插入数据小的, 然后插进去
             for(j = i-1; j>=0 && array[j] > key; j--) {
                 array[j+1] = array[j];
             }
@@ -17,7 +25,12 @@ public class InsertSort {
     }
 
     public static void main(String[] args) {
-        int[] array = {6,3,5,4,9,8,6,7};
+        int[] array = new int[100000];
+
+        Random random = new Random();
+        for(int i=0; i<100000; i++) {
+            array[i] = random.nextInt(100000);
+        }
         sort(array);
         for(int i : array) {
             System.out.println(i);
